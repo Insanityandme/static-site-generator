b1e620f26993916a4478abd47832b74c025bca4c
Insanityandme <bengtegardbook@gmail.com>
2024-04-15 22:41:21 +0200

    add content and template.html file

--- src/test_extract_page.py
+++ src/test_extract_page.py
@@ -4,10 +4,20 @@ from generate_page import extract_title, generate_page
 
 class TestExtractPage(unittest.TestCase):
     def test_extract_title(self):
-        text = "# I am correct"
+        text = """
+### I'm a heading
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+# I'm a heading
+
+* This is a list
+* with items
+        """
         result = extract_title(text)
 
-        self.assertEqual(result, "I am correct")
+        self.assertEqual(result, "I'm a heading")
 
     def test_generate_page(self):
         pass
